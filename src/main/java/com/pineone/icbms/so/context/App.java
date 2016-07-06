74863dc4c4a8bde06a1161c60883b5c3421d8cfa
existmaster <existmaster@gmail.com>
2016-07-06 19:08:09 +0900

    sample source commit

---
+++ src/main/java/com/pineone/icbms/so/context/App.java
@@ -0,0 +1,13 @@
+package com.pineone.icbms.so.context;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
