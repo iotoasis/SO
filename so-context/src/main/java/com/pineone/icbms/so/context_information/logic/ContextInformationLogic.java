3374cbd60cc98cb0c4bac7226e60eaba18744763
Melvin <oblivion1420@gmail.com>
2016-08-01 13:28:12 +0900

    ContextInformation Store 구현

--- so-context/src/main/java/com/pineone/icbms/so/context_information/logic/ContextInformationLogic.java
+++ so-context/src/main/java/com/pineone/icbms/so/context_information/logic/ContextInformationLogic.java
@@ -1,6 +1,8 @@
 package com.pineone.icbms.so.context_information.logic;
 
 import com.pineone.icbms.so.context_information.entity.ContextInformation;
+import com.pineone.icbms.so.context_information.store.ContextInformationMapStore;
+import com.pineone.icbms.so.context_information.store.ContextInformationStore;
 import com.pineone.icbms.so.context_information.temp.device.ConceptService;
 import com.pineone.icbms.so.context_information.temp.device.DeviceCenter;
 import com.pineone.icbms.so.context_information.temp.device.DeviceObject;
@@ -35,6 +37,7 @@ public class ContextInformationLogic {
 
     //NOTE: ContextInformation 등록정보를 수신받고 TODO : SO DB에 저장하고 SDA 에 등록하며 보여줘야할 내용(결정 필요)을 리턴
     public ResponseMessage registerGeneralContext(@RequestBody ContextInformation contextInformation){
+        ContextInformationStore contextInformationStore = ContextInformationMapStore.getInstance();
         DataValidation dataValidation = DataValidation.newGeneralContextValidation();
         contextInformation.setId("CI-" + contextInformation.getId());
         ResponseMessage responseMessage = ResponseMessage.newResponseMessage();
@@ -44,7 +47,7 @@ public class ContextInformationLogic {
             responseMessage.setExceptionMessage(e.getMessage());
             return responseMessage;
         }
-//      TODO : DB 저장
+        contextInformationStore.createContextInformation(contextInformation);
 //      TODO : SDA 에 등록
         responseMessage.setMessage(responseMessage.generalContextResultMessage(contextInformation));
         return responseMessage;
