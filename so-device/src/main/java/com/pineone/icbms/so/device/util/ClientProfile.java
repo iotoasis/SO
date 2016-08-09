1181a55ca5625d5816e5ac135112329d2145a91f
pahnjy <pahnjy@daum.net>
2016-08-09 19:06:43 +0900

    Device Proxy 부분 구현 - 현재 SDA 및 SI 서버가 안되어 있어서 임의의 테스트 코드 작성.

--- so-device/src/main/java/com/pineone/icbms/so/device/util/ClientProfile.java
+++ so-device/src/main/java/com/pineone/icbms/so/device/util/ClientProfile.java
@@ -6,7 +6,11 @@ public class ClientProfile {
     public static final String	SO_CONTROL_NOTIFICATON_URI	= "http://219.248.137.7:10080/so/resources/vdcm/";
     public static final String	SO_CONTROL_TYPE				= "text/plain:0";
     public static final String	CONTROL_ACTION				= "action";
+    public static final String  SI_CONTOL_URI               = "http://166.104.112.34:8081/si/command";
 
+
+    //SDA Data
+    public static final String  SDA_DATAREQUEST_URI         = "http://oasia.org/sda/hanyang/";
 
     // Presentation Data
     public static final String	RESPONSE_SUCCESS			= "success";
@@ -14,5 +18,6 @@ public class ClientProfile {
     public static final String	RESPONSE_SUCCESS_CODE		= "200";
     public static final String	RESPONSE_FIALURE_CODE		= "400";
     public static final String	RESPONSE_SUCCESS_ONEM2MCODE	= "2000";
+
 
 }
