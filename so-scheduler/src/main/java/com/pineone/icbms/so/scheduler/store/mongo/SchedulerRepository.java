f765a6dddce0965d029591aee6bfc282456a2312
Melvin <oblivion1420@gmail.com>
2016-09-26 14:25:21 +0900

    Scheduler 추가 요구사항 반영

--- so-scheduler/src/main/java/com/pineone/icbms/so/scheduler/store/mongo/SchedulerRepository.java
+++ so-scheduler/src/main/java/com/pineone/icbms/so/scheduler/store/mongo/SchedulerRepository.java
@@ -1,9 +1,6 @@
 package com.pineone.icbms.so.scheduler.store.mongo;
 
-import com.pineone.icbms.so.profile.store.mongo.ProfileDataObject;
-import com.pineone.icbms.so.scheduler.entity.ScheduledProfile;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.List;
 
