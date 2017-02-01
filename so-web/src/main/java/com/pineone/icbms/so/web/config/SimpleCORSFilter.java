69b2616de48b4eec99960c8c8ad2473207827475
Melvin <oblivion1420@gmail.com>
2017-02-01 17:35:27 +0900

    크로스도메인 이슈 해결 테스트

---
+++ so-web/src/main/java/com/pineone/icbms/so/web/config/SimpleCORSFilter.java
@@ -0,0 +1,33 @@
+package com.pineone.icbms.so.web.config;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by melvin on 2017. 2. 1..
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+
+        chain.doFilter(req, res);
+    }
+    public void init(FilterConfig filterConfig) {}
+    public void destroy() {}
+}
