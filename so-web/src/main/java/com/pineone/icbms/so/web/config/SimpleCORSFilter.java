3a6323462f97aa06686802684a83f291d0b4f796
Melvin <oblivion1420@gmail.com>
2017-02-01 17:51:31 +0900

    크로스 도메인 Header 내용 추가

--- so-web/src/main/java/com/pineone/icbms/so/web/config/SimpleCORSFilter.java
+++ so-web/src/main/java/com/pineone/icbms/so/web/config/SimpleCORSFilter.java
@@ -21,8 +21,7 @@ public class SimpleCORSFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
         response.setHeader("Access-Control-Allow-Origin", "*");
 
 
