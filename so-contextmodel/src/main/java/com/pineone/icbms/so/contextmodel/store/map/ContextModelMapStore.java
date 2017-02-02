8cc2691ec63d8aabf51d3cc7dc6cdf99a6a37b04
Melvin <oblivion1420@gmail.com>
2017-02-02 11:36:27 +0900

    중복내용 제거

--- so-contextmodel/src/main/java/com/pineone/icbms/so/contextmodel/store/map/ContextModelMapStore.java
+++ so-contextmodel/src/main/java/com/pineone/icbms/so/contextmodel/store/map/ContextModelMapStore.java
@@ -12,20 +12,18 @@ import java.util.Map;
  * Created by melvin on 2016. 8. 2..
  */
 
-public class ContextModelMapStore implements ContextModelStore {
+public class ContextModelMapStore {
     private static ContextModelMapStore instance;
     private ContextModelMapStore(){};
     private Map<String, ContextModel> contextModelStore = new HashMap<>();
 
     //NOTE: DB에 ContextModel 데이터 생성
-    @Override
     public void createContextModel(ContextModel contextModel) {
         //
         contextModelStore.put(contextModel.getName(), contextModel);
     }
 
     //NOTE: DB 에서 ContextModel List 조회
-    @Override
     public List<ContextModel> retrieveContextModelList() {
         List<ContextModel> contextModelList = new ArrayList<>();
         for(String key : contextModelStore.keySet()){
@@ -35,13 +33,11 @@ public class ContextModelMapStore implements ContextModelStore {
     }
 
     //NOTE: DB 에서 ContextModel 상세 조회
-    @Override
     public ContextModel retrieveContextModelDetail(String contextModelName) {
         ContextModel contextModel = contextModelStore.get(contextModelName);
         return contextModel;
     }
 
-    @Override
     public ContextModel retrieveContextModelDetailByName(String contextModelName) {
         return null;
     }
