db7b51e02f891b6d84e6d0c25becea187a5bbe02
gibubi <gibubi@daum.net>
2018-01-22 16:47:41 +0900

    script에 compositevirtualobject 추가

--- so-doc/scripts/create-topics-so.sh
+++ so-doc/scripts/create-topics-so.sh
@@ -6,6 +6,7 @@ echo .
 
 ${KAFKA_BIN_PATH}/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic contextmodel
 ${KAFKA_BIN_PATH}/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic orchestrationservice
+${KAFKA_BIN_PATH}/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic compositevirtualobject
 ${KAFKA_BIN_PATH}/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic virtualobject
 ${KAFKA_BIN_PATH}/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic devicecontrol
 ${KAFKA_BIN_PATH}/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic devicecontrol-seq
